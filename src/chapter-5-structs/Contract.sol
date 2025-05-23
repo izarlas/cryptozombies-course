2bafd5aba699d2fcd6cb3b4338f9308419eee1d4
Ioannis Zarlas <ioannis.zarlas@chromaway.com>
2025-05-23 17:03:34 +0300

    Lint staged

--- src/chapter-5-structs/Contract.sol
+++ src/chapter-5-structs/Contract.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.9;
 
 contract ZombieFactory {
-uint dnaDigits = 16;
-uint dnaModulus = 10 ** dnaDigits;
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
 
-struct Zombie {
-    string name;
-    uint dna;
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 }
-}
-
